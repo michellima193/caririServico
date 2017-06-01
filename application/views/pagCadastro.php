d4b770b2d2f5028e25d8e08c1c1d3c259e95d0e1
Michell <Michell@Michel>
2017-06-01 07:16:18 -0300

    michel pagina Principal 1.0/pagina da cadastro 1.0

--- application/views/pagCadastro.php
+++ application/views/pagCadastro.php
@@ -1,28 +1,73 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Cadastro Trabalhador</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('bootstrap-3.3.7-dist/css/bootstrap-theme.min'); ?>">
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('bootstrap-3.3.7-dist/css/bootstrap.css'); ?>">
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('bootstrap-3.3.7-dist/css/estilo2.css'); ?>">
+
+        <script src="<?php echo base_url("bootstrap-3.3.7-dist/js/jquery-3.2.1.min.js"); ?>"></script>
+        <script src="<?php echo base_url("bootstrap-3.3.7-dist/js/bootstrap.min.js"); ?>"></script>
     </head>
     <body>
         <?php echo form_open('ControllerCadastro/cadastro'); ?>
-        <input type="text" name="Nome_Usuario" placeholder="nome usuario" required=""/>
-        <input type="text" name="CPF" placeholder="CPF" required/>
-        <input type="text" name="Detalhe" placeholder="detalhe"/>
-        <input type="text" name="User_Login" placeholder="usuario"/>
-        <input type="text" name="Senha_Usuario" placeholder="senha"/>
-        <input type="text" name="CEP" placeholder="CEP"/>
-        <input type="text" name="Numero" placeholder="Nº"/>
-        <input type="text" name="Complemento" placeholder="Complemento"/>
-        <input type="text" name="Email" placeholder="e-mail"/>
-        <input type="text" name="Telefone1" placeholder="t1"/>
-        <input type="text" name="Telefone2" placeholder="t2"/>
-        <input type="submit" value="Enviar">
-        <?php echo form_close(); ?>
+        <div class="container" style="padding-top: 130px">
+            <div class="row">
+                <div class="col-xs-12 col-lg-10 col-lg-offset-1">
+                    <div class="form-group">
+                        <label for="Nome_Usuario">Nome:</label>
+                        <input type="text" class="form-control" name="Nome_Usuario" id="Nome_Usuario" placeholder="nome usuario" required=""/>
+                    </div>
+                    <div class="form-group">
+                        <label for="CPF">CPF:</label>
+                        <input type="text" class="form-control" name="CPF" id="CPF" placeholder="CPF" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Detalhe">Detalhe:</label>
+                        <input type="text" class="form-control" name="Detalhe"  id="Detalhe" placeholder="detalhe"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="User_Login">Nome Login</label>
+                            <input type="text" class="form-control" name="User_Login"  id="User_Login" placeholder="usuario"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Senha_Usuario">Senha:</label>
+                        <input type="password" class="form-control" name="Senha_Usuario" id="Senha_Usuario" placeholder="senha"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="CEP">CEP:</label>
+                        <input type="text" class="form-control" name="CEP" id="CEP" placeholder="CEP"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Numero">Número:</label>
+                        <input type="number" class="form-control" name="Numero" id="Numero" placeholder="Nº"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Complemento">Complemento:</label>
+                        <input type="text" class="form-control" name="Complemento"  id="Complemento" placeholder="Complemento"/>
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label for="Email">Email:</label>
+                        <input type="email" class="form-control" name="Email" id="Email" placeholder="e-mail"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Telefone1">Telefone 1 :</label>
+                        <input type="text" class="form-control" name="Telefone1" id="Telefone1" placeholder="t1"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="Telefone2">Telefone 2 :</label>
+                        <input type="text" class="form-control" name="Telefone2" id="Telefone2" placeholder="t2"/>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-block btn-primary" value="Enviar">
+                    </div>
+                    <?php echo form_close(); ?>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
